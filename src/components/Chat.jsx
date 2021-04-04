fe350425c813bc8c7fc8c6b0e2b79833fa38b0f6
yuuki-katsuta <reptpr0314@gmail.com>
2021-04-04 22:52:33 +0900

    use of useLocation

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,24 +1,23 @@
-import { Redirect } from 'react-router-dom';
+import { Redirect, useLocation } from 'react-router-dom';
 import MatchInformation from './MatchInformation';
 import MessageItem from './Message/MessageItem';
 import Container from '@material-ui/core/Container';
 import Score from './Score/Score.jsx';
 
-const Chat = ({ history, location }) => {
-  return location.state ? (
+const Chat = ({ history }) => {
+  const matchData = useLocation().state.matchData;
+  return matchData ? (
     <div className='container'>
       <Container maxWidth='md' disableGutters={true}>
-        <MatchInformation matchData={location.state.matchData} />
-        {location.state.matchData.scoreData && (
-          <Score matchData={location.state.matchData} />
-        )}
+        <MatchInformation matchData={matchData} />
+        {matchData.scoreData && <Score matchData={matchData} />}
       </Container>
       <Container
         maxWidth='lg'
         disableGutters={true}
         style={{ padding: '0 10px', marginTop: '40px' }}
       >
-        <MessageItem history={history} matchData={location.state.matchData} />
+        <MessageItem history={history} matchData={matchData} />
       </Container>
     </div>
   ) : (
