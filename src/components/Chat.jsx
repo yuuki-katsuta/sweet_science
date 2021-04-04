7f6cbd179076b45de1205a24cc044bb9700e9fec
yuuki-katsuta <reptpr0314@gmail.com>
2021-04-05 01:13:44 +0900

    url直打ち対処

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,20 +4,22 @@ import MessageItem from './Message/MessageItem';
 import Container from '@material-ui/core/Container';
 import Score from './Score/Score.jsx';
 
-const Chat = ({ history }) => {
-  const matchData = useLocation().state.matchData;
-  return matchData ? (
+const Chat = () => {
+  const location = useLocation();
+  return location.state ? (
     <div className='container'>
       <Container maxWidth='md' disableGutters={true}>
-        <MatchInformation matchData={matchData} />
-        {matchData.scoreData && <Score matchData={matchData} />}
+        <MatchInformation matchData={location.state.matchData} />
+        {location.state.matchData.scoreData && (
+          <Score matchData={location.state.matchData} />
+        )}
       </Container>
       <Container
         maxWidth='lg'
         disableGutters={true}
         style={{ padding: '0 10px', marginTop: '40px' }}
       >
-        <MessageItem history={history} matchData={matchData} />
+        <MessageItem matchData={location.state.matchData} />
       </Container>
     </div>
   ) : (
