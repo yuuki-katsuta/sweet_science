6719560ed0c0c260cabbd79668e904dbf136330e
yuuki-katsuta <reptpr0314@gmail.com>
2021-04-18 22:22:22 +0900

    共通レイアウト作成

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import PrivateRoute from './PrivateRoute';
 import { AuthProvider } from './auth/AuthProvider';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from './components/Header/Header';
 import Auth from './components/Auth/Auth';
 import routes from './router/routes';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Route exact path='/auth' component={Auth} />
-        <Header />
-        <Switch>
-          {routes.map((route) => (
-            <PrivateRoute
-              key={route.path}
-              exact
-              path={route.path}
-              component={route.component}
-              status={route.status}
-            />
-          ))}
-        </Switch>
+        <Layout>
+          <Switch>
+            {routes.map((route) => (
+              <PrivateRoute
+                key={route.path}
+                exact
+                path={route.path}
+                component={route.component}
+                status={route.status}
+              />
+            ))}
+          </Switch>
+        </Layout>
       </BrowserRouter>
     </AuthProvider>
   );
