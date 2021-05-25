ad4d70e3c1e384df660334c8ed6d1026fd9f9f9a
yuuki-katsuta <reptpr0314@gmail.com>
2021-05-25 23:58:13 +0900

    不要コード削除

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -2,13 +2,10 @@ import { useContext } from 'react';
 import { Route } from 'react-router-dom';
 import { RootContext } from './Provider';
 import Auth from './components/Auth/Auth';
-import Page404 from './components/Page404';
 
 const PrivateRoute = ({ component, path, status, ...options }) => {
   const { currentUser } = useContext(RootContext);
-  if (status === 404) return <Route component={Page404} />;
   const Component = currentUser ? component : Auth;
-
   return <Route {...options} path={path} component={Component} />;
 };
 
