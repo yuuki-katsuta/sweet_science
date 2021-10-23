8eba48184f21531ea95b84b1d5e1a488ccab1f84
yuuki-katsuta <reptpr0314@gmail.com>
2021-10-24 00:10:07 +0900

    不要ファイル削除

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import PrivateRoute from './PrivateRoute';
 import { Provider } from './Provider';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import routes from './router/routes';
 import Layout from './Layout/Layout';
 
+const options = {
+  timeout: 2500,
+  position: positions.BOTTOM_CENTER,
+  offset: '30px',
+};
+
 function App() {
   return (
-    <Provider>
-      <BrowserRouter>
-        <Layout>
-          <Switch>
-            {routes.map((route) => (
-              <PrivateRoute
-                key={route.path}
-                exact
-                path={route.path}
-                component={route.component}
-              />
-            ))}
-            <Route path='*' render={() => <Redirect to='/' />} />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
-    </Provider>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <Provider>
+        <BrowserRouter>
+          <Layout>
+            <Switch>
+              {routes.map((route) => (
+                <PrivateRoute
+                  key={route.path}
+                  exact
+                  path={route.path}
+                  component={route.component}
+                />
+              ))}
+              <Route path='*' render={() => <Redirect to='/' />} />
+            </Switch>
+          </Layout>
+        </BrowserRouter>
+      </Provider>
+    </AlertProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import './base.css';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { positions, Provider as AlertProvider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
-
-const options = {
-  timeout: 2500,
-  position: positions.BOTTOM_CENTER,
-  offset: '30px',
-};
 
 ReactDOM.render(
   <>
-    <AlertProvider template={AlertTemplate} {...options}>
-      <App />
-    </AlertProvider>
+    <App />
   </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
