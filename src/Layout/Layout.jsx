38750b56d5efd2cffe39abe905845c79ec6544e0
yuuki-katsuta <reptpr0314@gmail.com>
2022-01-08 21:17:31 +0900

    ページ遷移時ウィンドウ最上部から表示するよう変更

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,12 +2,14 @@ import Header from '../components/ui/Header/Header';
 import { useContext } from 'react';
 import { AuthStateContext } from '../providers/AuthStateProvider';
 import Auth from '../components/pages/Auth';
+import ScrollToTop from '../ScrollToTop';
 
 const Layout = ({ children }) => {
   const { currentUser } = useContext(AuthStateContext);
 
   return currentUser ? (
     <>
+      <ScrollToTop />
       <Header />
       {children}
     </>
