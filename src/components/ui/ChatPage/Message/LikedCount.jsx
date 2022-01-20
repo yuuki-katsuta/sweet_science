1501452c21b8e550a90aecc6a22d85e6c2be3af2
yuuki-katsuta <reptpr0314@gmail.com>
2022-01-20 23:13:34 +0900

    refactor

--- src/components/ui/ChatPage/Message/LikedCount.jsx
+++ src/components/ui/ChatPage/Message/LikedCount.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, memo, useRef } from 'react';
+import { useEffect, useState, memo, useRef, useMemo } from 'react';
 import { db } from '../../../../base';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -13,76 +13,70 @@ const SCount = styled.span`
 const LikedCount = memo(({ room, id, userUid, liked }) => {
   const [isLiked, setIsLiked] = useState(false);
   const processing = useRef(false);
+
+  const docRef = useMemo(
+    () => db.collection('chats').doc(room).collection('messages').doc(`${id}`),
+    [room, id]
+  );
+
   useEffect(() => {
     let isMounted = true;
-    const docRef = db
-      .collection('chats')
-      .doc(room)
-      .collection('messages')
-      .doc(`${id}`);
     (async () => {
       const likedUser = await docRef
         .collection('likedUser')
         .doc(`${userUid}`)
         .get();
-      //いいね済み
-      if (likedUser.exists && likedUser.data().user === userUid && isMounted)
-        setIsLiked(true);
+      isMounted &&
+        setIsLiked(likedUser.exists && likedUser.data().user === userUid);
     })();
-    return () => {
-      isMounted = false;
-    };
-  }, [id, room, userUid]);
+    return () => (isMounted = false);
+  }, [id, room, userUid, docRef]);
+
+  const removeUserLikes = async () => {
+    setIsLiked(false);
+    return await db
+      .runTransaction(async (transaction) => {
+        const latestgetData = await transaction.get(docRef);
+        const latestData = latestgetData.data().liked;
+        const newCount = latestData - 1;
+        transaction.update(docRef, { liked: newCount });
+      })
+      .then(async () => {
+        processing.current = false;
+        //いいねユーザーを削除
+        await docRef.collection('likedUser').doc(`${userUid}`).delete();
+      });
+  };
+
+  const registerUserLikes = async () => {
+    await docRef.collection('likedUser').doc(`${userUid}`).set({
+      user: userUid,
+      createdAt: new Date(),
+    });
+    return await db
+      .runTransaction(async (transaction) => {
+        const latestgetData = await transaction.get(docRef);
+        const latestData = latestgetData.data().liked;
+        const newCount = latestData + 1;
+        transaction.update(docRef, { liked: newCount });
+      })
+      .then(() => {
+        processing.current = false;
+        setIsLiked(true);
+      });
+  };
 
   const handleClick = async () => {
     //更新中なら処理しない
     if (processing.current) return;
     processing.current = true;
-    try {
-      const docRef = db
-        .collection('chats')
-        .doc(room)
-        .collection('messages')
-        .doc(`${id}`);
-      const likedUser = await docRef
-        .collection('likedUser')
-        .doc(`${userUid}`)
-        .get();
-      //いいね済み
-      if (likedUser.exists && likedUser.data().user === userUid) {
-        setIsLiked(false);
-        return await db
-          .runTransaction(async (transaction) => {
-            const latestgetData = await transaction.get(docRef);
-            const latestData = latestgetData.data().liked;
-            const newCount = latestData - 1;
-            transaction.update(docRef, { liked: newCount });
-          })
-          .then(async () => {
-            processing.current = false;
-            //いいねユーザーを削除
-            await docRef.collection('likedUser').doc(`${userUid}`).delete();
-          });
-      }
-      //いいねユーザーを登録
-      await docRef.collection('likedUser').doc(`${userUid}`).set({
-        user: userUid,
-        createdAt: new Date(),
-      });
-      return await db
-        .runTransaction(async (transaction) => {
-          const latestgetData = await transaction.get(docRef);
-          const latestData = latestgetData.data().liked;
-          const newCount = latestData + 1;
-          transaction.update(docRef, { liked: newCount });
-        })
-        .then(() => {
-          processing.current = false;
-          setIsLiked(true);
-        });
-    } catch (error) {
-      alert(error.message);
-    }
+    const likedUser = await docRef
+      .collection('likedUser')
+      .doc(`${userUid}`)
+      .get();
+    if (likedUser.exists && likedUser.data().user === userUid)
+      removeUserLikes();
+    else registerUserLikes();
   };
 
   return (
